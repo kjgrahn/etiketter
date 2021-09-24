4b92e4077136c89f2ba96795daaa774e27f56343
Jörgen Grahn <grahn+src@snipabacken.se>
2021-09-25 00:21:41 +0200

    decode UTF-8 to Latin-1

--- strings.cc
+++ strings.cc
@@ -3,10 +3,42 @@
  */
 #include "strings.h"
 #include "xml.h"
+#include "utf8.h"
+
+#include <cstring>
+#include <vector>
+#include <iterator>
 
 namespace {
 
     void nop(void*, const char*, ...) {}
+
+    /**
+     * Decoding UTF-8 to Latin-1 in XML content. Characters which do
+     * not fit (emojis, fancy quotes, foreign alphabets, ...) are
+     * replaced with a '?'.  Undefined result on broken UTF-8.
+     */
+    class Utf8 {
+    public:
+	std::string decode(xml::Node* node);
+
+    private:
+	std::vector<unsigned> v;
+    };
+}
+
+std::string Utf8::decode(xml::Node* node)
+{
+    const auto p = xmlNodeGetContent(node);
+    const auto q = p + std::strlen(cast(p));
+    v.clear();
+    auto it = std::back_inserter(v);
+    utf8::decode(p, q, it);
+    xmlFree(p);
+    for (auto& ch : v) {
+	if (ch>0xff) ch = '?';
+    }
+    return {v.begin(), v.end()};
 }
 
 using xlsx::SharedStrings;
@@ -20,9 +52,9 @@ SharedStrings::SharedStrings(const std::string& path)
     xmlXPathRegisterNs(ctx,  cast("ns"),
 		       cast("http://schemas.openxmlformats.org/spreadsheetml/2006/main"));
 
-    xml::xpath::Obj match {ctx, "/ns:sst/ns:si/ns:t"};
-    for (auto node: match) {
-	val.push_back(content(node));
+    Utf8 utf8;
+    for (auto node: xml::xpath::Obj{ctx, "/ns:sst/ns:si/ns:t"}) {
+	val.push_back(utf8.decode(node));
     }
     xmlXPathFreeContext(ctx);
     xmlFreeDoc(doc);
